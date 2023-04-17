6498e29f8b970544b81f0aecf03932862676d193
Tomislav Busic <tomislav.busic89@gmail.com>
2023-04-17 19:41:41 +0200

    run server

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log("Welcome world");
+// Import dependencies
+const express = require("express");
+
+// Create an express app
+const app = express();
+
+// Routing
+app.get("/", (req, res) => {
+  res.json("Hello world");
+});
+
+app.listen(3000);
