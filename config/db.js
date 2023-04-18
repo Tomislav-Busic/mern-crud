54081044b5b2202c56d069f2b97e4dd9e05bd9b0
Tomislav Busic <tomislav.busic89@gmail.com>
2023-04-18 21:16:14 +0200

    Connected to database

--- config/db.js
+++ config/db.js
@@ -1,7 +1,13 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  await mongoose.connect(process.env.DB_URL);
+    try {
+      await mongoose.connect(process.env.DB_URL);
+      console.log("Connected to database");
+    } catch (error) {
+      console.log(error);
+    }
+  
 };
 
 module.exports = connectDB;
