198799d90a10f8e96e0eb8d2004df148daac2870
Tomislav Busic <tomislav.busic89@gmail.com>
2023-04-18 21:23:56 +0200

    notes post

--- server.js
+++ server.js
@@ -16,4 +16,11 @@ app.get("/", (req, res) => {
   res.json({ hello: "World" });
 });
 
+app.post("/notes", (req, res) => {
+  // Get the sent in data off request body
+  // Create a note with it
+  // respond with the new note
+});
+
+
 app.listen(process.env.PORT);
