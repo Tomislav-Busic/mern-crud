211ad8838b02f78a59a05e11964314f69027938c
Tomislav Busic <tomislav.busic89@gmail.com>
2023-04-18 21:20:18 +0200

    note

--- models/note.js
+++ models/note.js
@@ -1,8 +1,13 @@
 const mongoose = require("mongoose");
 
-const noteSchema = mongoose.Schema({
-  title: String,
-  body: String,
-});
+const noteSchema = mongoose.Schema(
+  {
+    title: String,
+    body: String,
+  },
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("Note", noteSchema);
