42d56148a7b5c297b10819c3b2f18f04cbd28ffb
Tomislav Busic <tomislav.busic89@gmail.com>
2023-04-19 18:53:15 +0200

    Create a note with it

--- server.js
+++ server.js
@@ -6,12 +6,13 @@ if (process.env.NODE_ENV != "production") {
 // Import dependencies
 const express = require("express");
 const connectDB = require("./config/db");
+const Note = require("./models/note");
 
 // Create an express app
 const app = express();
 
 // Configure express app
-app.use(express.json())
+app.use(express.json());
 
 // Connect to database
 connectDB();
@@ -21,12 +22,17 @@ app.get("/", (req, res) => {
   res.json({ hello: "World" });
 });
 
-app.post("/notes", (req, res) => {
+app.post("/notes", async (req, res) => {
   // Get the sent in data off request body
   const title = req.body.title;
   const body = req.body.body;
-  
+
   // Create a note with it
+  await Note.create({
+    title: title,
+    body: body,
+  });
+
   // respond with the new note
 });
 
