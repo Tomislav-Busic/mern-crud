c84648fcab5d3d10e3f86821adea6ce0a625b8a5
Tomislav Busic <tomislav.busic89@gmail.com>
2023-04-20 20:14:00 +0200

    fetch the notes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,21 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 function App() {
+  const [notes, setNotes] = useState(null);
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  const fetchNotes = async () => {
+    // Fetch the notes
+    const res = await axios.get("http://localhost:3000/notes");
+    // Set to state
+    console.log(res);
+    setNotes(res.data.notes);
+  };
+
   return (
     <div>
       <h1>Hello World!</h1>
