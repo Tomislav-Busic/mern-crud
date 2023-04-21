5b24ce6f73d77b58fd78ca6848ae0e76ae0c78de
Tomislav Busic <tomislav.busic89@gmail.com>
2023-04-21 20:28:04 +0200

    notes.map

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,16 +12,14 @@ function App() {
     // Fetch the notes
     const res = await axios.get("http://localhost:3000/notes");
     // Set to state
-    console.log(res);
     setNotes(res.data.notes);
+    console.log(res);
   };
 
   return (
     <div>
       <h1>Hello World!</h1>
-      {notes.map((note) => (
-        <h2 key={note._id}>{note.title}</h2>
-      ))}
+      {notes && notes.map((note) => <h2 key={note._id}>{note.title}</h2>)}
     </div>
   );
 }
