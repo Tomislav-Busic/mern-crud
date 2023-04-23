ef803b119f3b73a4e9e87d0a5266652ef56192e1
Tomislav Busic <tomislav.busic89@gmail.com>
2023-04-23 20:06:41 +0200

    updateCreateFormField

--- frontend/src/stores/store.js
+++ frontend/src/stores/store.js
@@ -4,10 +4,24 @@ import axios from "axios";
 export const noteStore = create((set) => ({
   notes: null,
 
+  createForm: {
+    title: "",
+    body: "",
+  },
+
   fetchNotes: async () => {
     // Fetch the notes
     const res = await axios.get("http://localhost:3000/notes");
     // Set to state
     set({ notes: res.data.notes });
+  },
+  updateCreateFormField: (e) => {
+    const { name, value } = e.target;
+    set((state) => ({
+      createForm: {
+        ...state.createForm,
+        [name]: value,
+      },
+    }));
   },
 }));
