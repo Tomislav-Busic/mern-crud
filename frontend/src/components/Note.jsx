ca8c577be9ac79cd2dd013e68e867055964b7615
Tomislav Busic <tomislav.busic89@gmail.com>
2023-04-24 20:13:51 +0200

    Note

--- frontend/src/components/Note.jsx
+++ frontend/src/components/Note.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 
-export const Note = () => {
-  return <div>Note</div>;
+export const Note = (note) => {
+  return (
+    <div key={note._id}>
+      <h2>{note.title}</h2>
+      <p>Body: {note.body}</p>
+      <button onClick={() => store.deleteNote(note._id)}>
+        Delete {note.title}
+      </button>
+      <button onClick={() => store.toggleUpdate(note)}>Update</button>
+    </div>
+  );
 };
