fa6fef247bb1e3f7e2db0d030fc6ca12a32d5b0b
Tomislav Busic <tomislav.busic89@gmail.com>
2023-04-24 20:27:20 +0200

    CreateForm delete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,30 +13,7 @@ function App() {
   return (
     <div>
       <Notes />
-      <div>
-        <h2 style={{ textAlign: "center" }}>Create note</h2>
-        <form
-          onSubmit={store.createNote}
-          style={{
-            display: "flex",
-            alignItems: "center",
-            flexDirection: "column",
-            gap: "1rem",
-          }}
-        >
-          <input
-            name="title"
-            value={store.createForm.title}
-            onChange={store.updateCreateFormField}
-          />
-          <textarea
-            name="body"
-            value={store.createForm.body}
-            onChange={store.updateCreateFormField}
-          />
-          <button type="submit">Submit</button>
-        </form>
-      </div>
+
       <UpdateForm />
     </div>
   );
