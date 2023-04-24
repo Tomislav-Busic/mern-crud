51cb1ceccd3071a229ab09fc35c239657f945e37
Tomislav Busic <tomislav.busic89@gmail.com>
2023-04-24 20:10:14 +0200

    Notes.jsx

--- frontend/src/components/Notes.jsx
+++ frontend/src/components/Notes.jsx
@@ -3,5 +3,22 @@ import { noteStore } from "../stores/store";
 
 export const Notes = () => {
   const store = noteStore();
-  return <div>Notes</div>;
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      {store.notes &&
+        store.notes.map((note) => {
+          return (
+            <div key={note._id}>
+              <h2>{note.title}</h2>
+              <p>Body: {note.body}</p>
+              <button onClick={() => store.deleteNote(note._id)}>
+                Delete {note.title}
+              </button>
+              <button onClick={() => store.toggleUpdate(note)}>Update</button>
+            </div>
+          );
+        })}
+    </div>
+  );
 };
