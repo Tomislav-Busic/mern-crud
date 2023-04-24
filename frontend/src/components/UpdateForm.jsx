64208369fcb24401e4b5b83d7417601f80b6dca0
Tomislav Busic <tomislav.busic89@gmail.com>
2023-04-24 20:24:46 +0200

    updateForm

--- frontend/src/components/UpdateForm.jsx
+++ frontend/src/components/UpdateForm.jsx
@@ -1,5 +1,35 @@
 import React from "react";
+import { noteStore } from "../stores/store";
 
 export const UpdateForm = () => {
-  return <div>UpdateForm</div>;
+  const store = noteStore();
+
+  if (!store.updateForm._id) return <></>;
+
+  return (
+    <div>
+      <h2 style={{ textAlign: "center" }}>Update note</h2>
+      <form
+        onSubmit={store.onSubmitUpdate}
+        style={{
+          display: "flex",
+          alignItems: "center",
+          flexDirection: "column",
+          gap: "1rem",
+        }}
+      >
+        <input
+          name="title"
+          value={store.updateForm.title}
+          onChange={store.onChangeUpdate}
+        />
+        <textarea
+          name="body"
+          value={store.updateForm.body}
+          onChange={store.onChangeUpdate}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 };
