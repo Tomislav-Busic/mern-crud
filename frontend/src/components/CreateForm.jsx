7191dd014655dcc650adb680f5965c809632c2c2
Tomislav Busic <tomislav.busic89@gmail.com>
2023-04-24 20:28:58 +0200

    CreateForm.jsx

--- frontend/src/components/CreateForm.jsx
+++ frontend/src/components/CreateForm.jsx
@@ -1,5 +1,35 @@
 import React from "react";
+import { noteStore } from "../stores/store";
 
 export const CreateForm = () => {
-  return <div>CreateForm</div>;
+  const store = noteStore();
+
+  if (store.updateForm._id) return <></>;
+
+  return (
+    <div>
+      <h2 style={{ textAlign: "center" }}>Create note</h2>
+      <form
+        onSubmit={store.createNote}
+        style={{
+          display: "flex",
+          alignItems: "center",
+          flexDirection: "column",
+          gap: "1rem",
+        }}
+      >
+        <input
+          name="title"
+          value={store.createForm.title}
+          onChange={store.updateCreateFormField}
+        />
+        <textarea
+          name="body"
+          value={store.createForm.body}
+          onChange={store.updateCreateFormField}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 };
