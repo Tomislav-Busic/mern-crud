279cda104f6089ffc79b76cd359f39e079c0e260
Tomislav Busic <tomislav.busic89@gmail.com>
2023-04-24 20:09:45 +0200

    remove notes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
+import { useEffect } from "react";
 import { noteStore } from "./stores/store";
 
 function App() {
@@ -11,20 +10,6 @@ function App() {
 
   return (
     <div>
-      <h1>Hello World!</h1>
-      {store.notes &&
-        store.notes.map((note) => {
-          return (
-            <div key={note._id}>
-              <h2>{note.title}</h2>
-              <p>Body: {note.body}</p>
-              <button onClick={() => store.deleteNote(note._id)}>
-                Delete {note.title}
-              </button>
-              <button onClick={() => store.toggleUpdate(note)}>Update</button>
-            </div>
-          );
-        })}
       <div>
         <h2 style={{ textAlign: "center" }}>Create note</h2>
         <form
--- frontend/src/components/Notes.jsx
+++ frontend/src/components/Notes.jsx
@@ -1,5 +1,7 @@
 import React from "react";
+import { noteStore } from "../stores/store";
 
 export const Notes = () => {
+  const store = noteStore();
   return <div>Notes</div>;
 };
