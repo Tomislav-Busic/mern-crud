3184eaaec9898aa22b3ec6998f0ff0e0644f938e
Tomislav Busic <tomislav.busic89@gmail.com>
2023-04-25 19:13:48 +0200

    user routing

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,7 @@ const {
   updateNote,
   deleteNote,
 } = require("./controllers/notesController");
+const usersController = require("./controllers/usersController");
 
 // Create an express app
 const app = express();
@@ -26,15 +27,14 @@ app.use(cors());
 connectDB();
 
 // Routing
+app.post("/signup", usersController.signup);
+app.post("/login", usersController.login);
+app.get("/logout", usersController.logout);
 
 app.get("/notes", getAllNotes);
-
 app.get("/notes/:id", getNoteById);
-
 app.post("/notes", createNewNote);
-
 app.put("/notes/:id", updateNote);
-
 app.delete("/notes/:id", deleteNote);
 
 app.listen(process.env.PORT);
