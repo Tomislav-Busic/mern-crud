5036132c933e0147a6a337a1e2f6630c408541cd
Tomislav Busic <tomislav.busic89@gmail.com>
2023-04-25 19:09:34 +0200

    usersController

---
+++ backend/controllers/usersController.js
@@ -0,0 +1,9 @@
+const User = require("../models/user");
+
+const signup = (req, res) => {};
+
+const login = (req, res) => {};
+
+const logout = (req, res) => {};
+
+module.exports = { signup, login, logout };
