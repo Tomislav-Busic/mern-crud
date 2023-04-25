8d99eb1633f71d641fae13fb50b9fde75b693fcc
Tomislav Busic <tomislav.busic89@gmail.com>
2023-04-25 19:56:29 +0200

    try, catch

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -2,17 +2,23 @@ const User = require("../models/user");
 const bcrypt = require("bcryptjs");
 
 const signup = async (req, res) => {
-  // Get the email and pasword from req body
-  const { email, password } = req.body;
+    try {
+      // Get the email and pasword from req body
+      const { email, password } = req.body;
 
-  // Hash password
-  const hashedPassword = bcrypt.hashSync(password, 8);
+      // Hash password
+      const hashedPassword = bcrypt.hashSync(password, 8);
 
-  // Create a user with req data
-  await User.create({ email, password: hashedPassword });
+      // Create a user with req data
+      await User.create({ email, password: hashedPassword });
 
-  // Response
-  res.status(201).json({ data: req.body });
+      // Response
+      res.status(201).json({ data: req.body });
+    } catch (error) {
+      res.status(400);
+      console.log(error);
+    }
+  
 };
 
 const login = (req, res) => {};
