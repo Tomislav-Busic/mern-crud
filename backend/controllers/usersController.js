84fc5a50a709cce21ae945d88e83726b8245f2b2
Tomislav Busic <tomislav.busic89@gmail.com>
2023-04-25 19:40:11 +0200

    hashedPassword

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -1,12 +1,17 @@
 const User = require("../models/user");
+const bcrypt = require("bcryptjs");
 
 const signup = async (req, res) => {
   // Get the email and pasword from req body
   const { email, password } = req.body;
 
-  // Create a user with req data
-  await User.create({ email, password });
+  // Hash password
+  const hashedPassword = bcrypt.hashSync(password, 8);
 
+  // Create a user with req data
+  await User.create({ email, password: hashedPassword });
+
+  // Response
   res.status(201).json({ data: req.body });
 };
 
