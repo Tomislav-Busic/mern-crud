90247034b80f1e29dfeb1d8fe19c205d37c94489
Tomislav Busic <tomislav.busic89@gmail.com>
2023-04-26 19:54:27 +0200

    jwt token response

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -36,7 +36,9 @@ const login = async (req, res) => {
   // Create a jwt token
   const exp = Date.now() + 1000 * 60 * 60 * 24 * 30;
   const token = jwt.sign({ sub: user._id, exp }, process.env.SECRET_JWT);
+
   // Send it
+  res.json({ token });
 };
 
 const logout = (req, res) => {};
