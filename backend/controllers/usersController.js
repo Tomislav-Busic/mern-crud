6c8dfe003476840e95a6da71ca2128c3162291d4
Tomislav Busic <tomislav.busic89@gmail.com>
2023-04-26 19:42:56 +0200

    create a jwt token

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -1,24 +1,24 @@
 const User = require("../models/user");
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 
 const signup = async (req, res) => {
-    try {
-      // Get the email and pasword from req body
-      const { email, password } = req.body;
+  try {
+    // Get the email and pasword from req body
+    const { email, password } = req.body;
 
-      // Hash password
-      const hashedPassword = bcrypt.hashSync(password, 8);
+    // Hash password
+    const hashedPassword = bcrypt.hashSync(password, 8);
 
-      // Create a user with req data
-      await User.create({ email, password: hashedPassword });
+    // Create a user with req data
+    await User.create({ email, password: hashedPassword });
 
-      // Response
-      res.status(201).json({ data: req.body });
-    } catch (error) {
-      console.log(error);
-      res.sendStatus(400);
-    }
-  
+    // Response
+    res.status(201).json({ data: req.body });
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(400);
+  }
 };
 
 const login = async (req, res) => {
@@ -32,7 +32,9 @@ const login = async (req, res) => {
   // Compare send in password with found user password hash
   const passwordMatch = bcrypt.compareSync(password, user.password);
   if (!passwordMatch) return res.sendStatus(401);
+
   // Create a jwt token
+  const token = jwt.sign({ foo: "bar" }, privateKey, { algorithm: "RS256" });
   // Send it
 };
 
