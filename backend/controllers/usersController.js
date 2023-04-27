d04e944d44afa3d37d5214acb9f77712c674d797
Tomislav Busic <tomislav.busic89@gmail.com>
2023-04-27 19:21:41 +0200

    cookie parser

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -34,16 +34,16 @@ const login = async (req, res) => {
   if (!passwordMatch) return res.sendStatus(401);
 
   // Create a jwt token (better version down)
-   const exp = Date.now() + 1000 * 60 * 60 * 24 * 30;
-   /*const token = jwt.sign({ sub: user._id, exp }, process.env.SECRET_JWT); */
+  const exp = Date.now() + 1000 * 60 * 60 * 24 * 30;
+  const token = jwt.sign({ sub: user._id, exp }, process.env.SECRET_JWT);
 
-   // Set the cookie
-   res.cookie("Authorization", generateToken(user._id), {
-     /*  expires:  new Date(exp)*/
-     httpOnly: true,
-     sameSite: "lax",
-     secure: process.env.NODE_ENV === "production",
-   });
+  // Set the cookie
+  res.cookie("Authorization", token, {
+    expires: new Date(exp),
+    httpOnly: true,
+    sameSite: "lax",
+    secure: process.env.NODE_ENV === "production",
+  });
 
   // Send it
   res.status(200).json({ token });
@@ -51,11 +51,11 @@ const login = async (req, res) => {
 
 const logout = (req, res) => {};
 
-// Generate JWT 
+// Generate JWT
 const generateToken = (id) => {
-    return jwt.sign({id}, process.env.JWT_SECRET, {
-        expiresIn: "30d"
-    })
-}
+  return jwt.sign({ id }, process.env.SECRET_JWT, {
+    expiresIn: "30d",
+  });
+};
 
 module.exports = { signup, login, logout };
