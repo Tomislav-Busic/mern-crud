c0ff07446c9e65186a351d3d57d643d3fe7eb9c3
Tomislav Busic <tomislav.busic89@gmail.com>
2023-04-27 19:46:16 +0200

    Read token off cookies

--- backend/middleware/requireAuth.js
+++ backend/middleware/requireAuth.js
@@ -2,7 +2,9 @@ const jwt = require("jsonwebtoken");
 
 const requireAuth = (req, res, next) => {
   // Read token off cookies
+  const token = req.cookie.Authorization;
   // Decode the token
+  const decoded = jwt.verify(token, "shhhhh");
   // Find user using decoded sub
   // Atach user to req
   // Continue on
