7ceaebc96b28aec509967891c72b737874f288c2
Tomislav Busic <tomislav.busic89@gmail.com>
2023-04-27 19:44:01 +0200

    desc

--- backend/middleware/requireAuth.js
+++ backend/middleware/requireAuth.js
@@ -1,6 +1,12 @@
 const jwt = require("jsonwebtoken");
 
 const requireAuth = (req, res, next) => {
+  // Read token off cookies
+  // Decode the token
+  // Find user using decoded sub
+  // Atach user to req
+  // Continue on
+
   console.log("In middleware");
   next();
 };
