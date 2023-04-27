474f86b58b602b9d67da62c046248ad08ed2a3f4
Tomislav Busic <tomislav.busic89@gmail.com>
2023-04-27 19:51:52 +0200

    Find user using decoded sub

--- backend/middleware/requireAuth.js
+++ backend/middleware/requireAuth.js
@@ -1,22 +1,25 @@
 const jwt = require("jsonwebtoken");
+const User = require("../models/user");
 
-const requireAuth = (req, res, next) => {
-    try {
-      // Read token off cookies
-      const token = req.cookie.Authorization;
-      // Decode the token
-      const decoded = jwt.verify(token, process.env.SECRET_JWT);
+const requireAuth = async (req, res, next) => {
+  try {
+    // Read token off cookies
+    const token = req.cookie.Authorization;
 
-      // Find user using decoded sub
-      // Atach user to req
-      // Continue on
+    // Decode the token
+    const decoded = jwt.verify(token, process.env.SECRET_JWT);
 
-      console.log("In middleware");
-      next();
-    } catch (error) {
-      res.sendStatus(401);
-    }
-  
+    // Find user using decoded sub
+    const user = await User.findById(decoded.sub);
+
+    // Atach user to req
+    // Continue on
+
+    console.log("In middleware");
+    next();
+  } catch (error) {
+    res.sendStatus(401);
+  }
 };
 
 module.exports = requireAuth;
