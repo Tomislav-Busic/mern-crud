f93d29f4b299b7b547dd106abeacadf23d02b7e2
Tomislav Busic <tomislav.busic89@gmail.com>
2023-04-28 20:04:10 +0200

    import notesPage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,9 @@
-import { useEffect } from "react";
-import { noteStore } from "./stores/store";
-import { Notes } from "./components/Notes";
-import { UpdateForm } from "./components/UpdateForm";
-import { CreateForm } from "./components/CreateForm";
+import { NotesPage } from "./pages/NotesPage";
 
 function App() {
-  const store = noteStore();
-
-  useEffect(() => {
-    store.fetchNotes();
-  }, []);
-
   return (
     <div>
-      <Notes />
-      <CreateForm />
-      <UpdateForm />
+      <NotesPage />
     </div>
   );
 }
