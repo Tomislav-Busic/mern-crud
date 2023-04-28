d9252b64c4388a673c90432219386ab6667e70c7
Tomislav Busic <tomislav.busic89@gmail.com>
2023-04-28 21:34:04 +0200

    import RequireAuth

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import { Navigation } from "./components/Navigation";
+import { RequireAuth } from "./components/RequireAuth";
 import { LoginPage } from "./pages/LoginPage";
 import { NotesPage } from "./pages/NotesPage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -9,7 +10,15 @@ function App() {
       <Router>
         <Navigation />
         <Routes>
-          <Route index path="/" element={<NotesPage />} />
+          <Route
+            index
+            path="/"
+            element={
+              <RequireAuth>
+                <NotesPage />
+              </RequireAuth>
+            }
+          />
           <Route path="/login" element={<LoginPage />} />
         </Routes>
       </Router>
