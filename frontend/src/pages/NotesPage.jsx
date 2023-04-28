a85725c0afac4de5406ea00b2391305a1cbeb997
Tomislav Busic <tomislav.busic89@gmail.com>
2023-04-28 20:03:23 +0200

    NotesPage

--- frontend/src/pages/NotesPage.jsx
+++ frontend/src/pages/NotesPage.jsx
@@ -1,5 +1,22 @@
-import React from "react";
+import { useEffect } from "react";
+import { noteStore } from "../stores/store";
+
+import { Notes } from "../components/Notes";
+import { CreateForm } from "../components/CreateForm";
+import { UpdateForm } from "../components/UpdateForm";
 
 export const NotesPage = () => {
-  return <div>NotesPage</div>;
+  const store = noteStore();
+
+  useEffect(() => {
+    store.fetchNotes();
+  }, []);
+
+  return (
+    <div>
+      <Notes />
+      <CreateForm />
+      <UpdateForm />
+    </div>
+  );
 };
