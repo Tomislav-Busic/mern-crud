287ddb85ea6bf60066a0daaa946d84470ea3bec0
Tomislav Busic <tomislav.busic89@gmail.com>
2023-04-28 21:41:19 +0200

    checkAuth

--- frontend/src/components/RequireAuth.jsx
+++ frontend/src/components/RequireAuth.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { authStore } from "../stores/authStore";
 
 export const RequireAuth = (props) => {
-  const state = authStore();
-  if (!state.loggedIn) {
+  const store = authStore();
+
+  useEffect(() => {
+    if (store.loggedIn === null) {
+      store.checkAuth();
+    }
+  }, []);
+  if (!store.loggedIn) {
     return <h1>Please login</h1>;
   }
   return <div>{props.children}</div>;
