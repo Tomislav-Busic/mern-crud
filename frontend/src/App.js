5b5ace595874c6a4911d485f12b5e3496dcd6a9c
Tomislav Busic <tomislav.busic89@gmail.com>
2023-04-28 20:10:57 +0200

    Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,16 @@
+import { LoginPage } from "./pages/LoginPage";
 import { NotesPage } from "./pages/NotesPage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <NotesPage />
+      <Router>
+        <Routes>
+          <Route index path="/" element={<NotesPage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
