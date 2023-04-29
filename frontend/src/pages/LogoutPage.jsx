1a4ac5ab79d02e48269f859f8e702cc80562fd76
Tomislav Busic <tomislav.busic89@gmail.com>
2023-04-29 20:21:25 +0200

    logout page

--- frontend/src/pages/LogoutPage.jsx
+++ frontend/src/pages/LogoutPage.jsx
@@ -1,5 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { authStore } from "../stores/authStore";
 
 export const LogoutPage = () => {
-  return <div>LogoutPage</div>;
+  const store = authStore();
+
+  useEffect(() => {
+    store.logout();
+  }, []);
+
+  return <div>You are now logged out</div>;
 };
