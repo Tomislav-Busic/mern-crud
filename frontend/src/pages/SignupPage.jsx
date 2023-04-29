9659b158648baec40bd15cabc48c08ba1dcb61ab
Tomislav Busic <tomislav.busic89@gmail.com>
2023-04-29 13:50:10 +0200

    import SignUpForm

--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { SignupForm } from "../components/SignUpForm";
+import { SignupForm } from "../components/SignupForm";
 
 export const SignupPage = () => {
   return (
