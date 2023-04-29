d2f6d839f727b195fea2f0bc988a1bb3f1d7f6f7
Tomislav Busic <tomislav.busic89@gmail.com>
2023-04-29 13:37:17 +0200

    signup form

--- frontend/src/components/SignUpForm.jsx
+++ frontend/src/components/SignUpForm.jsx
@@ -1,5 +1,28 @@
 import React from "react";
+import { authStore } from "../stores/authStore";
 
-export const SignUpForm = () => {
-  return <div>SignUpForm</div>;
+export const SignupForm = () => {
+  const store = authStore();
+
+  const handleSignup = async (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <form onSubmit={handleSignup}>
+      <input
+        type="email"
+        name="email"
+        value={store.signupForm.email}
+        onChange={store.updateSignupForm}
+      />
+      <input
+        type="password"
+        name="password"
+        value={store.signupForm.password}
+        onChange={store.updateSignupForm}
+      />
+      <button type="submit">Signup</button>
+    </form>
+  );
 };
