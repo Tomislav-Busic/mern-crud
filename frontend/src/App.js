69717898b88259744a58f862ee3d275133ffbcdd
Tomislav Busic <tomislav.busic89@gmail.com>
2023-04-29 13:47:46 +0200

    import SignUpPage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { RequireAuth } from "./components/RequireAuth";
 import { LoginPage } from "./pages/LoginPage";
 import { NotesPage } from "./pages/NotesPage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { SignupPage } from "./pages/SignupPage";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
             }
           />
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
         </Routes>
       </Router>
     </div>
